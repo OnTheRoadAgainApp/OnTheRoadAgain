3ad96c93e7a26774fe506f31560127b40f192c34
tcolejimenez <taylor.cole.jimenez@gmail.com>
2018-11-09 09:37:34 -0600

    began working on forms

--- src/main/java/works/ontheroadagain/app/controllers/VehicleController.java
+++ src/main/java/works/ontheroadagain/app/controllers/VehicleController.java
@@ -3,19 +3,37 @@ package works.ontheroadagain.app.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import works.ontheroadagain.app.models.User;
+import works.ontheroadagain.app.models.Vehicle;
+import works.ontheroadagain.app.services.VehicleRepository;
 import works.ontheroadagain.app.services.VehicleService;
 
 @Controller
 public class VehicleController {
-    private final VehicleService vehicleSvc;
+//    private final VehicleService vehicleSvc;
+    private final VehicleRepository vehicleRepo;
 
-    public VehicleController(VehicleService vehicleSvc) {
-        this.vehicleSvc = vehicleSvc;
+    public VehicleController(VehicleRepository vehicleRepo) {
+        this.vehicleRepo = vehicleRepo;
     }
 
     @GetMapping("/vehicles")
     public String vehicles(Model m) {
-        m.addAttribute("vehicles", vehicleSvc.findAll());
+        m.addAttribute("vehicles", vehicleRepo.findAll());
+        return "/vehicles";
+    }
+
+    @GetMapping("addVehicle")
+    public String showCreateForm(Model model) {
+        model.addAttribute("vehicle", new Vehicle());
+        return "/addVehicle";
+    }
+
+    @PostMapping("addVehicle")
+    public String create(@ModelAttribute Vehicle vehicle) {
+        vehicleRepo.save(vehicle);
         return "/vehicles";
     }
 }
