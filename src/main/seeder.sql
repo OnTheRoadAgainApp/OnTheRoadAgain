a4a11ccd86642371f2d84e2fd240f7bb0a83cf12
Michael Strezishar <michael.strezishar@gmail.com>
2018-11-13 11:24:43 -0600

    Progress bar test and added inserts into seeder file

--- src/main/seeder.sql
+++ src/main/seeder.sql
@@ -5,5 +5,5 @@ INSERT INTO users (address, city, email, first_name, last_name, password, phone_
 ('540 Good Rd.', 'San Antonio', 'willv@auto.com', 'Will', 'Verudian', 'will', 5429128456, 'TX', 'willv', 78323, 'TECHNICIAN'),
 ('9834 Fun Prom.','San Antonio','jerry@auto.com','Jerry','Mouse','jerry', 9782341836,'TX', 'jerrym', 78934,'TECHNICIAN');
 
-INSERT INTO service_types (id, description) VALUES (1, 'Lube, Oil, and Filter'),( 2, 'Tire Rotation'),(3, 'Fuel System Service'), (4, 'Engine Air Filer Replacement'),
+INSERT INTO service_types (id, description) VALUES (1, 'Lube, Oil, and Filter'),( 2, 'Tire Rotation'),(3, 'Fuel System Service'), (4, 'Engine Air Filter Replacement'),
 (5, 'Scheduled Maintenance by Mileage'), (6, 'Battery Replacement'), ( 7, 'Brake Service'), (8, 'Diagnostics'), (9, 'Wheel Alignment' );
