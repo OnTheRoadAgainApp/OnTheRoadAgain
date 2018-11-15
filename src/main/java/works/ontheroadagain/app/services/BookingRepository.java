afb5498b66e11878c1a984be76fb2e961b7e8737
tamisharutledge <tamisha.rutledge@gmail.com>
2018-11-14 19:22:11 -0600

    technicians can see booking and personal info is displayed.

--- src/main/java/works/ontheroadagain/app/services/BookingRepository.java
+++ src/main/java/works/ontheroadagain/app/services/BookingRepository.java
@@ -2,6 +2,7 @@ package works.ontheroadagain.app.services;
 
 import org.springframework.data.repository.CrudRepository;
 import works.ontheroadagain.app.models.ServiceBooking;
+import works.ontheroadagain.app.models.User;
 import works.ontheroadagain.app.models.Vehicle;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 public interface BookingRepository extends CrudRepository<ServiceBooking, Long> {
     ServiceBooking findByVehicle(Vehicle vehicle);
     List<ServiceBooking> findAllByVehicle(Vehicle vehicle);
+    List<ServiceBooking> findAllByTechnician(User user);
+    ServiceBooking findById(Long id);
 }
