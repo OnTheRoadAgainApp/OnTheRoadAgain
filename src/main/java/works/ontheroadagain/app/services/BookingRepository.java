9545d1a8b38c8f3ed859265dd7815ef0c7ce5e34
Michael Strezishar <michael.strezishar@gmail.com>
2018-11-16 14:34:30 -0600

    updated booking repo

--- src/main/java/works/ontheroadagain/app/services/BookingRepository.java
+++ src/main/java/works/ontheroadagain/app/services/BookingRepository.java
@@ -2,6 +2,7 @@ package works.ontheroadagain.app.services;
 
 import org.springframework.data.repository.CrudRepository;
 import works.ontheroadagain.app.models.ServiceBooking;
+import works.ontheroadagain.app.models.User;
 import works.ontheroadagain.app.models.Vehicle;
 
 import java.util.List;
@@ -9,5 +10,7 @@ import java.util.List;
 public interface BookingRepository extends CrudRepository<ServiceBooking, Long> {
 
       List<ServiceBooking> findAllByVehicle (Vehicle vehicle);
+      List<ServiceBooking> findAllByTechnician (User user);
+      ServiceBooking findById (Long id);
 
 }
