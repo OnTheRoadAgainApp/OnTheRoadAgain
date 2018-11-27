8d1c214f11c15eb4de33a2f7c53c1646cce67193
tcolejimenez <taylor.cole.jimenez@gmail.com>
2018-11-27 10:22:43 -0600

    automatic redirect working

--- src/main/java/works/ontheroadagain/app/controllers/DefaultController.java
+++ src/main/java/works/ontheroadagain/app/controllers/DefaultController.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import works.ontheroadagain.app.models.User;
+import works.ontheroadagain.app.models.UserWithRoles;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -12,8 +13,9 @@ public class DefaultController {
     @GetMapping("/default")
     public String defaultAfterLogin(HttpServletRequest request) {
 
-        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-//        Long role = currentUser.getRole().getId();
+        UserWithRoles currentUser = (UserWithRoles) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        String role = currentUser.getUserRole();
+
         System.out.println(role);
         if (role.equals("TECHNICIAN")) {
             return "redirect:/technicians";
@@ -21,10 +23,10 @@ public class DefaultController {
         if (role.equals("ADVISOR")) {
             return "redirect:/advisor";
         }
-        if (role.equals("CUSTOMERR")) {
+        if (role.equals("CUSTOMER")) {
             return "redirect:/profile";
         }
 
-        return "/";
+        return "home";
     }
 }
