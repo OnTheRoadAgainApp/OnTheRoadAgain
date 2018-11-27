25e1c1e8096c3f7f9c778e200139ff5b623bce2f
Michael Strezishar <michael.strezishar@gmail.com>
2018-11-27 15:17:48 -0600

    added payment received page for stripe api

--- src/main/java/works/ontheroadagain/app/services/SmsSender.java
+++ src/main/java/works/ontheroadagain/app/services/SmsSender.java
@@ -9,17 +9,17 @@ import org.springframework.stereotype.Service;
 public class SmsSender {
     // Find your Account Sid and Auth Token at twilio.com/console
     public static final String ACCOUNT_SID =
-            "AC49b748cc9ea5b7a25ec107cab88ce718";
+            "AC52a50f412791bf9099a83097e8db28e0";
     public static final String AUTH_TOKEN =
-            "276b08043ee32fa6d2bd911664628cbb";
+            "b155eb32997dd68c88950a5fa51c9497";
 
 //    public static void main(String[] args) {
         public void sendText(){
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
 
         Message message = Message
-                .creator(new PhoneNumber("+12104152689"), // to
-                        new PhoneNumber("+18302660961"), // from
+                .creator(new PhoneNumber("+15122307292"), // to
+                        new PhoneNumber("+15128725950"), // from
                         "Your vehicle is ready for pickup!")
                 .create();
 
