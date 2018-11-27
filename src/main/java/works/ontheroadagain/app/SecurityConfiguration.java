ae188978762be42300102a68c7f4969ca9888243
tcolejimenez <taylor.cole.jimenez@gmail.com>
2018-11-26 19:32:29 -0600

    tables mobile responsive ~~~

--- src/main/java/works/ontheroadagain/app/SecurityConfiguration.java
+++ src/main/java/works/ontheroadagain/app/SecurityConfiguration.java
@@ -37,7 +37,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 /* Login configuration */
                 .formLogin()
                 .loginPage("/login")
-                .defaultSuccessUrl("/") // user's home page, it can be any URL
+                .defaultSuccessUrl("/") // user's home page, it can be any URL "/decide"
                 .permitAll() // Anyone can go to the login page
 
                 /* Logout configuration */
